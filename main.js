5ece802d7e957859a6400167d295eef56fa1dd00
Johana Ludwig <johanaludwig@gmail.com>
2019-03-16 17:42:39 +0100

    main js setup

---
+++ main.js
@@ -0,0 +1,16 @@
+// Search Filter Function
+const input = document.getElementById('filter');
+const entry = document.getElementsByClassName('image');
+
+input.addEventListener("keyup", () => { 
+    const value = input.value;
+    
+    for (let i = 0; i < entry.length; i++) {
+      const description = entry[i].getAttribute("data-title");
+       
+      if (description.toLowerCase().indexOf(value.toLowerCase()) != -1) { 
+        entry[i].style.display = "block";
+      } else entry[i].style.display = "none";
+    }
+
+});
